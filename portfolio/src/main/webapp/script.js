fa9bd716ce10deb72a012a434273788f07a3ee4a
jnnybk <jennb206@gmail.com>
2020-06-03 13:58:50 +0000

    Change dictionaries using string keys to real objects, remove window.onload content into document event listener on 'DOMContentLoaded'

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,31 +11,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-const imgFilePath = '/images/album/';
+const imgFilePath = '/images/album';
 const images = [ 
   {
-    'filePath': `${imgFilePath}LoveIsABeautifulThing_Vulfpeck.jpg`,
-    'link': 'https://youtu.be/x0vgMl6n7m0',
+    filePath: `${imgFilePath}/LoveIsABeautifulThing_Vulfpeck.jpg`,
+    link: 'https://youtu.be/x0vgMl6n7m0',
   },
   {
-    'filePath': `${imgFilePath}Mayday_Crush.jpg`, 
-    'link': 'https://youtu.be/29ycT6fA-Rs',
+    filePath: `${imgFilePath}/Mayday_Crush.jpg`,
+    link: 'https://youtu.be/29ycT6fA-Rs',
   },
   {
-    'filePath': `${imgFilePath}OjitosSonados_Ramona.jpg`, 
-    'link': 'https://youtu.be/-lJzIUniJOE',
+    filePath: `${imgFilePath}/OjitosSonados_Ramona.jpg`,
+    link: 'https://youtu.be/-lJzIUniJOE',
   },
   {
-    'filePath': `${imgFilePath}Rach2_AnnaFedorova.jpg`,
-    'link': 'https://youtu.be/rEGOihjqO9w',
+    filePath: `${imgFilePath}/Rach2_AnnaFedorova.jpg`,
+    link: 'https://youtu.be/rEGOihjqO9w',
   },
   {
-    'filePath': `${imgFilePath}SeTeOlvida_Ramona.jpg`,
-    'link': 'https://youtu.be/PIndisFYGB8',
+    filePath: `${imgFilePath}/SeTeOlvida_Ramona.jpg`,
+    link: 'https://youtu.be/PIndisFYGB8',
   },
   {
-    'filePath': `${imgFilePath}TunnelOfLove_haroinfather.jpg`,
-    'link': 'https://youtu.be/cdlvLZqT3Ok',
+    filePath: `${imgFilePath}/TunnelOfLove_haroinfather.jpg`,
+    link: 'https://youtu.be/cdlvLZqT3Ok',
   }];
 
 class ImageAnchors {
@@ -62,18 +62,16 @@ class ImageAnchors {
         this.index++;
       }
     }
-    document.albumSlide.src = images[this.index]['filePath'];
-    document.getElementById('wrapper').href = images[this.index]['link'];
+    document.albumSlide.src = images[this.index].filePath;
+    document.getElementById('wrapper').href = images[this.index].link;
   }
 
 }
 document.addEventListener('DOMContentLoaded', () => {
   const imgAnchor = new ImageAnchors();
+  document.albumSlide.src = images[0].filePath;
+  document.getElementById('wrapper').href = images[0].link;
 });
-window.onload = () => {
-  document.albumSlide.src = images[0]['filePath'];
-  document.getElementById('wrapper').href = images[0]['link'];
-}
 function setVisibility(currentValue, idName) {
   if (idName === currentValue) {
     document.getElementById(currentValue).style.display = "block";
