fe1cf9c503cab03b9f6c81e5712c6583a4aa6a33
jnnybk <jennb206@gmail.com>
2020-06-15 22:08:52 +0000

    Fix pull request changes, create function to hide / show elements, create class element to store buttons

--- portfolio/src/main/webapp/pagination.js
+++ portfolio/src/main/webapp/pagination.js
@@ -23,49 +23,58 @@ class CommentButton {
     this.totalNumberOfPagesRequested = 1;
     this.lastPage = -1;
     this.cursorString = null;
-     
-    document.getElementById('prev-comments-button').classList.add("hidden");
-    document.getElementById('end-of-comments-sign').classList.add("hidden");
+    this.prevCommentsButton = document.getElementById('prev-comments-button');
+    this.nextCommentsButton = document.getElementById('next-comments-button');
+    this.endOfCommentsSign = document.getElementById('end-of-comments-sign');
+    this.comments = document.getElementById('comments');
+
+    this.prevCommentsButton.classList.add("hidden");
+    this.endOfCommentsSign.classList.add("hidden");
    
     document.getElementById('maxNum').addEventListener('input', () => {
-      document.getElementById('comments').innerHTML = "";
-      document.getElementById('prev-comments-button').classList.add("hidden");
-      if (document.getElementById('next-comments-button').classList.contains("hidden")) {
-        document.getElementById('next-comments-button').classList.remove("hidden");
-      }
-      document.getElementById('end-of-comments-sign').classList.add("hidden");
+      this.comments.innerHTML = "";
+      this.prevCommentsButton.classList.add("hidden");
+      this.nextCommentsButton.classList.remove("hidden");
+      this.endOfCommentsSign.classList.add("hidden");
+
       this.currentCommentsPageIndex = 0;
       this.totalNumberOfPagesRequested = 1;
       this.lastPage = -1;
       this.requestAndDisplayNewCommentsPage();
-
     });
 
-    document.getElementById('prev-comments-button').addEventListener('click', () => {
+    this.prevCommentsButton.addEventListener('click', () => {
       this.displayPrevPage();
     });
 
-    document.getElementById('next-comments-button').addEventListener('click', () => {
+    this.nextCommentsButton.addEventListener('click', () => {
       this.displayNextPage();
     });
   }
 
-  displayNextPage() {
-    if (document.getElementById('prev-comments-button').classList.contains("hidden")) {
-      document.getElementById('prev-comments-button').classList.remove("hidden");
-    }
+  hideCommentAtIndex(index) {
+    const commentList = this.comments.children;
+    commentList[index].classList.add("hidden");
+  }
 
-    let commentList = document.getElementById('comments').children;
+  showCommentAtIndex(index) {
+    const commentList = this.comments.children;
+    commentList[index].classList.remove("hidden");
+  }
+
+  displayNextPage() {
+    this.prevCommentsButton.classList.remove("hidden");
+
+    let commentList = this.comments.children;
     if (this.currentCommentsPageIndex + 1 === this.lastPage) {
-        document.getElementById('next-comments-button').classList.add("hidden");
-        document.getElementById('end-of-comments-sign').classList.remove("hidden");
+      this.nextCommentsButton.classList.add("hidden");
+      this.endOfCommentsSign.classList.remove("hidden");
     }
     if (this.currentCommentsPageIndex + 1 < this.totalNumberOfPagesRequested) {
-      commentList[this.currentCommentsPageIndex+1].classList.remove("hidden");
-      commentList[this.currentCommentsPageIndex].classList.add("hidden");
-      
+      this.hideCommentAtIndex(this.currentCommentsPageIndex+1);
+      this.showCommentAtIndex(this.currentCommentsPageIndex);
     } else {
-      commentList[this.currentCommentsPageIndex].classList.add("hidden");
+      this.hideCommentAtIndex(this.currentCommentsPageIndex);
       this.requestAndDisplayNewCommentsPage(this.cursorString);
       this.totalNumberOfPagesRequested++;
     }
@@ -73,18 +82,14 @@ class CommentButton {
   }
 
   displayPrevPage() {
-    if (!document.getElementById('end-of-comments-sign').classList.contains("hidden")) {
-      document.getElementById('end-of-comments-sign').classList.add("hidden");
-    }
-    if (document.getElementById('next-comments-button').classList.contains("hidden")) {
-      document.getElementById('next-comments-button').classList.remove("hidden");
-    }
+    this.endOfCommentsSign.classList.add("hidden");
+    this.nextCommentsButton.classList.remove("hidden");
     
-    let commentList = document.getElementById('comments').children;
+    const commentList = this.comments.children;
     commentList[this.currentCommentsPageIndex-1].classList.remove("hidden");
     commentList[this.currentCommentsPageIndex].classList.add("hidden");
     if (this.currentCommentsPageIndex-1 === 0) {
-      document.getElementById('prev-comments-button').classList.add("hidden");
+      this.prevCommentsButton.classList.add("hidden");
     }
     this.currentCommentsPageIndex--;
   }
@@ -94,18 +99,18 @@ class CommentButton {
 
     const response = await fetch(`/data?max_num_of_comments=${maxNumberOfComments}&cursor=${cursorStr}`);
     const commentData = await response.json();
-    const commentListElement = document.getElementById('comments');
+    const commentListElement = this.comments;
     this.cursorString = commentData['cursorString'];
 
     let commentList= commentListElement.children;
 
     // If the fetched data has fewer number of comments than the maximum number of comments, it signals that there are no more comments.
     if (commentData['commentList'].length < maxNumberOfComments) {
-      document.getElementById('next-comments-button').classList.add("hidden");
-      document.getElementById('end-of-comments-sign').classList.remove("hidden");
+      this.nextCommentsButton.classList.add("hidden");
+      this.endOfCommentsSign.classList.remove("hidden");
       
       if (commentListElement.childElementCount !== 0){
-        document.getElementById('prev-comments-button').classList.remove("hidden");
+        this.prevCommentsButton.classList.remove("hidden");
       }
       this.lastPage = commentListElement.childElementCount;
     }
